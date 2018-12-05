f86753912ff62efa567b54f287a692e0b1477792
Hung Nguyen Van <nguyenvanhung.13t4@gmail.com>
2018-12-05 09:53:57 +0700

    init app

---
+++ src/main/java/com/journaldev/my_example/my_example_app/App.java
@@ -0,0 +1,13 @@
+package com.journaldev.my_example.my_example_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
